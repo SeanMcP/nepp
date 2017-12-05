31237c69795a837ef887f930958dd5f9f9928b9a
Sean McPherson <seanmcp25@gmail.com>
2017-12-04 21:21:23 -0500

    Add passport functions to server

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/index');
 const morgan = require('morgan');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const User = require('./models/index').User;
 const app = express();
 
 app.use(bodyParser.json());
@@ -10,6 +14,50 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(morgan('dev'));
+
+// Passport
+app.use(passport.initialize());
+app.use(passport.session());
+app.use((req, res, next) => {
+  res.locals.errorMessage = req.flash('error');
+});
+
+passport.use(new LocalStrategy(
+  (username, password, done) => {
+    User.findOne({
+      where: { 'username': username }
+    })
+    .then(user => {
+      if (user == null) {
+        return done(null, false, { message: 'Incorrect credentials.' });
+      }
+
+      let hashedPassword = bcrypt.hashSync(password, user.salt);
+
+      if (user.hashedPassword === hashedPassword) {
+        return done(null, user);
+      }
+
+      return done(null, false, { message: 'Incorrect credentials.' });
+    })
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findOne({
+    where: { 'id': id }
+  })
+  .then(user => {
+    if (user == null) {
+      done(new Error('Wrong user id.'));
+    }
+    done(null, user);
+  })
+})
 
 app.use(routes);
 
